7ad2cad68a9599331f9e001fede4168773e95fb3
Ronnasayd Machado <ronnasayd@hotmail.com>
2020-05-31 13:04:22 -0300

    feat(backend): add route to posts

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 const userRouter = require('./Routes/userRouter')
+const postRouter = require('./Routes/postRouter')
 
 app.use(express.json())
 
 // Route for users
 app.use('/users', userRouter)
+
+// Route for post
+app.use('/posts', postRouter)
 
 app.listen(3000)
