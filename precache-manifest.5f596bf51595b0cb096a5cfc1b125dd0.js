3d92e12d9c35896360ece33aa4e2c8baaf0ec60e
Ronnasayd <ronnasayd@hotmail.com>
2020-06-05 16:49:58 -0300

    Updates

---
+++ precache-manifest.5f596bf51595b0cb096a5cfc1b125dd0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2534f580fb9f53467e105cc6e3bcb46",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eb868bd8749250a16d85",
+    "url": "./static/js/2.d53f078c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.d53f078c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7048120cbc3574534d06",
+    "url": "./static/js/main.cf6b5f65.chunk.js"
+  },
+  {
+    "revision": "e736dabb8ee024a7b980",
+    "url": "./static/js/runtime-main.3993e964.js"
+  },
+  {
+    "revision": "be92f76f60b9e4a11a684707c960b2d3",
+    "url": "./static/media/github.be92f76f.svg"
+  },
+  {
+    "revision": "59370966508b58d26406663faf0d9e08",
+    "url": "./static/media/group-chat.59370966.svg"
+  },
+  {
+    "revision": "d505e0b4d95412694528e39a08ab2078",
+    "url": "./static/media/group.d505e0b4.svg"
+  },
+  {
+    "revision": "fea69764616e06ac71e3c4b022df1a05",
+    "url": "./static/media/linkedin.fea69764.svg"
+  },
+  {
+    "revision": "4c3521ef130041a31bcfed268d2fe874",
+    "url": "./static/media/location.4c3521ef.svg"
+  },
+  {
+    "revision": "164bf61d80cd548c2f3ba82420ad8f11",
+    "url": "./static/media/person.164bf61d.svg"
+  },
+  {
+    "revision": "2f3e45974f2b2033c6a9423a17814379",
+    "url": "./static/media/posts.2f3e4597.svg"
+  },
+  {
+    "revision": "ae1b2382059a100aa1db2cae9d85022f",
+    "url": "./static/media/up48.ae1b2382.svg"
+  }
+]);
