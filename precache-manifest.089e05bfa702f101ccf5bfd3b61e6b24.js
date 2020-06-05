92d909a3daf657627eb05d71de36a49ed488b858
Ronnasayd <ronnasayd@hotmail.com>
2020-06-05 16:20:24 -0300

    Updates

---
+++ precache-manifest.089e05bfa702f101ccf5bfd3b61e6b24.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb1e7d368c8fcc3700aab2466970e7f",
+    "url": "/Test_GrowthTech/index.html"
+  },
+  {
+    "revision": "747b2c5c9c50248aa773",
+    "url": "/Test_GrowthTech/static/js/2.a2b99f58.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Test_GrowthTech/static/js/2.a2b99f58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cda4a3a621638c7c705b",
+    "url": "/Test_GrowthTech/static/js/main.6dd79083.chunk.js"
+  },
+  {
+    "revision": "b8c882d8bd5780983f38",
+    "url": "/Test_GrowthTech/static/js/runtime-main.3bff80ad.js"
+  },
+  {
+    "revision": "be92f76f60b9e4a11a684707c960b2d3",
+    "url": "/Test_GrowthTech/static/media/github.be92f76f.svg"
+  },
+  {
+    "revision": "59370966508b58d26406663faf0d9e08",
+    "url": "/Test_GrowthTech/static/media/group-chat.59370966.svg"
+  },
+  {
+    "revision": "d505e0b4d95412694528e39a08ab2078",
+    "url": "/Test_GrowthTech/static/media/group.d505e0b4.svg"
+  },
+  {
+    "revision": "fea69764616e06ac71e3c4b022df1a05",
+    "url": "/Test_GrowthTech/static/media/linkedin.fea69764.svg"
+  },
+  {
+    "revision": "4c3521ef130041a31bcfed268d2fe874",
+    "url": "/Test_GrowthTech/static/media/location.4c3521ef.svg"
+  },
+  {
+    "revision": "164bf61d80cd548c2f3ba82420ad8f11",
+    "url": "/Test_GrowthTech/static/media/person.164bf61d.svg"
+  },
+  {
+    "revision": "2f3e45974f2b2033c6a9423a17814379",
+    "url": "/Test_GrowthTech/static/media/posts.2f3e4597.svg"
+  },
+  {
+    "revision": "ae1b2382059a100aa1db2cae9d85022f",
+    "url": "/Test_GrowthTech/static/media/up48.ae1b2382.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9a40f3ca05fcef5d816d15430c25ce57.js"
+  "/Test_GrowthTech/precache-manifest.089e05bfa702f101ccf5bfd3b61e6b24.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Test_GrowthTech/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
